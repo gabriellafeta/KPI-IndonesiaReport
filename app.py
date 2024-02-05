e069dc12a624e044cbb082d86f360f5dd080e327
Gabriel Lafetá <gabiellafetnascimento@gmail.com>
2024-02-05 19:32:19 -0300

    commit

--- app.py
+++ app.py
@@ -1,8 +1,67 @@
 # importando arquivos
 import pandas as pd
 import streamlit as st
+from azure.storage.blob import BlobServiceClient, BlobClient
+from azure.core.exceptions import ResourceExistsError
+import os
+from io import StringIO
+
+#------------------------------------------------------------------------------------------------------
+# Uploading data
+
+connection_string = "DefaultEndpointsProtocol=https;AccountName=beesexpansion0001;AccountKey=QBAsqeUnSwNe7hKHJwWrKfH1XE0LpERqc/N/x5jg51pKCvoOgaZw0NvIgxKwyciZ2JxnnjdBbu0b+ASt9jRAaA==;EndpointSuffix=core.windows.net"
+blob_service_client = BlobServiceClient.from_connection_string(connection_string)
+
+container_name = 'expansionbees0001'
+local_file_path = r'C:\Users\gabri\OneDrive\Área de Trabalho\DataID'
+#blob_name = 'blob0001'
+
+container_client = blob_service_client.get_container_client(container_name)
+
+# Create a blob client using the local file name as the name for the blob
+blob_client = blob_service_client.get_blob_client(container=container_name, blob=blob_name)
+
+# Upload the file
+
+container_client = blob_service_client.get_container_client(container_name)
+try:
+    container_client.create_container()
+except ResourceExistsError:
+    print(f"Container '{container_name}' already exists.")
+
+
+# Iterate through the files and upload each one
+overwrite = False
+
+# for root, dirs, files in os.walk(local_file_path):
+#     for file in files:
+#         file_path = os.path.join(root, file)
+#         blob_path = os.path.relpath(file_path, local_file_path).replace(os.sep, '/')
+
+#         try:
+#             blob_client = container_client.get_blob_client(blob=blob_path)
+#             with open(file_path, "rb") as data:
+#                 blob_client.upload_blob(data, overwrite=overwrite)
+#             print(f"Uploaded '{file_path}' to Blob '{blob_path}' in container '{container_name}'.")
+#         except ResourceExistsError:
+#             print(f'Blob "{blob_path}" already exists, skipping.')
+
+
+#------------------------------------------------------------------------------------------------------
+# Tables
+blob_name = 'export.csv'
+
+blob_client = blob_service_client.get_blob_client(container=container_name, blob=blob_name)
+
+blob_content = blob_client.download_blob().content_as_text()
+
+data = StringIO(blob_content)
+df = pd.read_csv(data)
+
+
+#------------------------------------------------------------------------------------------------------
 
 # app
 
 st.write('Hello World!')
-st.write('Hello World!')
+st.dataframe(df)
