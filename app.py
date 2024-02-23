4a0e1fe13991b904ce45eac3fc14cafc015f0163
Gabriel Lafetá <gabiellafetnascimento@gmail.com>
2024-02-23 10:16:22 -0300

    commit

--- app.py
+++ app.py
@@ -153,7 +153,8 @@ kpi1_all_barplot.update_layout(
     xaxis=dict(tickmode='linear', title=''),
     showlegend=False,
     yaxis=dict(showgrid=False, showticklabels=False, title=''),  # Hide Y-axis grid lines and tick labels
-    plot_bgcolor='white'  # Set background color to white for a clean look
+    plot_bgcolor='white',
+    margin=dict(t=50)  # Set background color to white for a clean look
 )
 
 kpi1_all_barplot.update_traces(
@@ -188,7 +189,8 @@ kpi1_bram_barplot.update_layout(
     xaxis=dict(tickmode='linear', title=''),
     showlegend=False,
     yaxis=dict(showgrid=False, showticklabels=False, title=''),  # Hide Y-axis grid lines and tick labels
-    plot_bgcolor='white'  # Set background color to white for a clean look
+    plot_bgcolor='white',
+    margin=dict(t=50)  # Set background color to white for a clean look
 )
 
 kpi1_bram_barplot.update_traces(
@@ -203,6 +205,9 @@ kpi1_bram_barplot.update_layout(
     width=500,  # Adjust the width to fit within the column
     height=300  # You can also adjust the height if necessary
 )
+
+###### Harris
+
 
 #------------------------------------------------------------------------------------------------------
 #### App
