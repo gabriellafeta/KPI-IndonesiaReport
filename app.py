137154f6d834cdfd8d2da200e6cd2a504f7903ae
Gabriel Lafetá <gabiellafetnascimento@gmail.com>
2024-02-23 18:04:57 -0300

    commit

--- app.py
+++ app.py
@@ -696,7 +696,6 @@ kpi3_barplot_cum.update_layout(
 def formata_numero(valor, prefixo=''):
     return f'{prefixo}{valor / 1000000:.2f} mi PHP'
 
-
 df_t3['gmv_placed_customer'] = pd.to_numeric(df_t3['gmv_placed_customer'], errors='coerce').fillna(0)
 df_t3['gmv_placed_force'] = pd.to_numeric(df_t3['gmv_placed_force'], errors='coerce').fillna(0)
 df_t3['gmv_placed_grow'] = pd.to_numeric(df_t3['gmv_placed_grow'], errors='coerce').fillna(0)
@@ -704,13 +703,15 @@ df_t3['gmv_placed_grow'] = pd.to_numeric(df_t3['gmv_placed_grow'], errors='coerc
 df_t3['TOTAL_SALES'] = df_t3['gmv_placed_customer'] + df_t3['gmv_placed_force'] + df_t3['gmv_placed_grow']
 df_t3['TOTAL_SALES'] = pd.to_numeric(df_t3['TOTAL_SALES'], errors='coerce').fillna(0)
 
-df_t3_sales = df_t3.groupby('bdr_id')['TOTAL_SALES'].sum().reset_index().sort_values(by='TOTAL_SALES', ascending=False)
+df_t3_sales = df_t3.groupby('bdr_id')['TOTAL_SALES'].sum().reset_index()
 df_t3_sales_notnull = df_t3_sales[(df_t3_sales['TOTAL_SALES'] != 0)]
 df_t3_sales_notnull.dropna(subset=['TOTAL_SALES'], inplace=True)
 
-df_t3_sales_notnull['TOTAL_SALES'] = df_t3_sales_notnull['TOTAL_SALES'].fillna(0).apply(formata_numero, prefixo='')
+df_t3_sales_notnull_sort = df_t3_sales_notnull.sort_values(by='TOTAL_SALES', ascending=False)
 
-kpi4_all_barplot_bdr = px.bar(df_t3_sales_notnull, x='bdr_id', y='TOTAL_SALES', color_discrete_sequence=['LightSalmon'])
+df_t3_sales_notnull_sort['TOTAL_SALES'] = df_t3_sales_notnull_sort['TOTAL_SALES'].fillna(0).apply(formata_numero, prefixo='')
+
+kpi4_all_barplot_bdr = px.bar(df_t3_sales_notnull_sort, x='bdr_id', y='TOTAL_SALES', color_discrete_sequence=['LightSalmon'])
 
 
 kpi4_all_barplot_bdr.update_layout(
