d82468097b5738b11284294d6608783bebfa8389
Gabriel Lafetá <gabiellafetnascimento@gmail.com>
2024-02-26 15:40:25 -0300

    commit

--- app.py
+++ app.py
@@ -1256,8 +1256,9 @@ html_t5 = df_estilizado_t5.to_html()
 ###### Joined Force kpi
 
 df_joined = df_t4_grouped_sort.join(df_t5_grouped_sort, how='outer', lsuffix='_t4', rsuffix='_t5')
-cols_joined = cols_t4 + cols_t5
-df_estilizado_joined = style_table(df_joined, cols_joined)
+df_joined_sort = df_joined.sort_values(by='TOTAL_TASKS', ascending=False)
+df_joined_sort = cols_t4 + cols_t5
+df_estilizado_joined = style_table(df_joined, df_joined_sort)
 
 # Convert the styled DataFrame to HTML
 force_html = df_estilizado_joined.to_html()
