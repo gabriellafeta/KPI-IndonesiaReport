f55b77711dd31358da246d6a9b4321d312723e4e
Gabriel Lafetá <gabiellafetnascimento@gmail.com>
2024-02-26 16:01:37 -0300

    commit

--- app.py
+++ app.py
@@ -1271,73 +1271,65 @@ df_t4_stacked_bar = df_t4_sort.groupby(['FORMATTED_DATE', 'BDR_name'])['TOTAL_TA
 df_t4_stacked_bar['DATE_FOR_SORTING'] = pd.to_datetime(df_t4_stacked_bar['FORMATTED_DATE'], format='%d-%b')
 df_t4_pivot = df_t4_stacked_bar.pivot(index='DATE_FOR_SORTING', columns='BDR_name', values='TOTAL_TASKS').fillna(0)
 
-df_t4_stacked_line = df_t4_sort.groupby(['FORMATTED_DATE', 'BDR_name'])['TASK_EFFECTIVENESS'].mean().reset_index()
-df_t4_stacked_line['DATE_FOR_SORTING'] = pd.to_datetime(df_t4_stacked_line['FORMATTED_DATE'], format='%d-%b')
-df_t4_pivot_line = df_t4_stacked_line.pivot(index='DATE_FOR_SORTING', columns='BDR_name', values='TASK_EFFECTIVENESS').fillna(0)
-df_t4_pivot_line.index = df_t4_pivot_line.index.strftime('%d-%b')
-
-
 df_t4_pivot.index = df_t4_pivot.index.strftime('%d-%b')
 tasks_stacked = go.Figure()
 
 blue_palette = ['#1f77b4', '#aec7e8', '#c6dbef', '#6baed6', '#2171b5', '#4c78a8', '#9ecae1']
 
 for i, vendor in enumerate(df_t4_pivot.columns):
+    text_labels = [f'{v}' if v != '0' else '' for v in df_t4_pivot[vendor]]
+
     tasks_stacked.add_trace(go.Bar(
         x=df_t4_pivot.index, 
         y=df_t4_pivot[vendor], 
         name=vendor,
         marker_color=blue_palette[i % len(blue_palette)],  # Use the color palette
-        text=[f'{v}' if v != 0 else '' for v in df_t4_pivot[vendor]],  # Use values as text labels
-        textposition='outside',  # Position labels outside the bars
-        hoverinfo='text',  # Set hover info
-        # Bar plot is associated with the left y-axis by default
+        text=text_labels,  # Use the prepared text labels
+        textposition='outside'  # Position labels outside the bars
     ))
 
-# Add line traces for TASK_EFFECTIVENESS, associated with the secondary Y-axis
-for i, vendor in enumerate(df_t4_pivot_line.columns):
-    line_color = f"rgba({255 - i*30}, {100 + i*30}, {100 + i*20}, 1)"
-    tasks_stacked.add_trace(go.Scatter(
-        x=df_t4_pivot_line.index,
-        y=df_t4_pivot_line[vendor],
-        name=f"{vendor} Effectiveness",
-        mode='lines+markers',
-        line=dict(color=line_color),
-        hoverinfo='y',
-        yaxis='y2'  # Associate this trace with the secondary y-axis
-    ))
+tasks_stacked.update_layout(barmode='stack', title='Daily Tasks by BDR', xaxis_title='', yaxis_title='')
+for i, trace in enumerate(tasks_stacked.data):
+    trace.text = [f'{v}' if v != 0 else '' for v in df_t2_pivot[trace.name]]
 
-# Update the layout to accommodate the stacked bars and secondary Y-axis for percentages
+# Customizing the figure's layout
 tasks_stacked.update_layout(
     barmode='stack',
-    title='Daily Tasks and Effectiveness by BDR',
-    xaxis=dict(
-        tickangle=-90,
-        tickmode='linear',
-        dtick=1,
-        type='category'
-    ),
-    yaxis=dict(
-        title='Total Tasks',
-        showgrid=False,  # Hide grid lines for the primary Y-axis
-    ),
-    yaxis2=dict(
-        title='Task Effectiveness (%)',
-        overlaying='y',  # This specifies that yaxis2 is overlaying the primary y-axis
-        side='right',  # Position the secondary Y-axis on the right
-        showgrid=False,  # Optionally hide grid lines for the secondary Y-axis
-        range=[0, 1]  # Since it's a percentage, the range is set from 0 to 100
-    ),
-    legend=dict(
-        orientation='h',
-        yanchor='bottom',
-        y=1.02,
-        xanchor='right',
-        x=1
-    ),
-    plot_bgcolor='rgba(0,0,0,0)'
+    title='Daily Visits by BDR',
+    xaxis_title='',
+    yaxis_title='',
+    xaxis_tickangle=-90,
+    yaxis={'visible': False, 'showticklabels': False},
+    plot_bgcolor='rgba(0,0,0,0)',
+    xaxis={'showgrid': False},
 )
 
+
+tasks_stacked.update_layout(
+    xaxis=dict(
+        tickmode='linear',
+        dtick=1,  # Set the interval between ticks to 1 day
+        tickangle=-90,  # Rotate labels by 90 degrees
+        type='category'  # This ensures that all categories (dates) are displayed
+    ),
+    yaxis=dict(
+        showticklabels=False,  # Hide Y-axis labels
+        showgrid=False,  # Hide grid lines
+    ),
+    plot_bgcolor='rgba(0,0,0,0)',  # Transparent background
+    barmode='stack',
+    title='Daily Registers by BDR',
+    showlegend=True,
+    legend=dict(
+        orientation='h',
+        yanchor='top',
+        y=-0.2,  # You might need to adjust this value to fit your chart
+        xanchor='center',
+        x=0.5  # Center the legend on the x-axis
+    ),
+    margin=dict(b=50),
+    height=600
+    )
 #------------------------------------------------------------------------------------------------------
 #### App
 # Abas
