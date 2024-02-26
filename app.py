ec291eab6f01ae32802ca582d256785cf84bcbc8
Gabriel Lafetá <gabiellafetnascimento@gmail.com>
2024-02-26 09:37:25 -0300

    commit

--- app.py
+++ app.py
@@ -520,6 +520,9 @@ df_t1['FORMATTED_DATE'] = df_t1['VISIT_DATE'].dt.strftime('%d-%b')
 df_t1_stacked = df_t1.groupby(['FORMATTED_DATE', 'BDR_name'])['visits_format'].sum().reset_index()
 df_t1_pivot = df_t1_stacked.pivot(index='FORMATTED_DATE', columns='BDR_name', values='visits_format').fillna(0)
 
+df_t1_pivot.sort_index(inplace=True)
+df_t1_pivot.index = df_t1_pivot.index.strftime('%d-%b')
+
 visits_stacked = go.Figure()
 colors = px.colors.sequential.Blues
 
