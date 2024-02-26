c75cf0b9f33ad87d05b8e38605dd7fa7b7648d32
Gabriel Lafetá <gabiellafetnascimento@gmail.com>
2024-02-26 14:23:40 -0300

    commit

--- app.py
+++ app.py
@@ -1100,13 +1100,13 @@ for i, vendor in enumerate(df_t3_pivot_gmvbdr.columns):
         hovertext=hover_texts
     ))
 
-for trace in df_t3_pivot_gmvbdr.data:
+for trace in gmvbdr_stacked.data:
     formatted_text = [custom_format(value) if value != 0 else '' for value in trace.y]
     trace.update(
         hoverinfo='text',
         hovertext=[f"<b>{x}</b><br>{trace.name}: {custom_format(y)} PHP" for x, y in zip(df_t3_pivot_gmvbdr['FORMATTED_DATE'], trace.y)],
         text=formatted_text,
-        texttemplate='%{text}',  # Use the formatted text
+        texttemplate='%{text}',
         textposition='outside'
     )
 
