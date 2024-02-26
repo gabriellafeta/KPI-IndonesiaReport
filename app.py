bac74a2b816ca89df1515192cb7ac9ffdd229d4d
Gabriel Lafetá <gabiellafetnascimento@gmail.com>
2024-02-26 08:21:13 -0300

    commit

--- app.py
+++ app.py
@@ -547,7 +547,7 @@ kpi2_barplot_dateagg.update_layout(
 
 # KPI 2 per BDR
 
-df_aggregated_t2_BDR = df_t2_sorted.groupby('bdr_name')['count_registered_stores'].sum().reset_index()
+df_aggregated_t2_BDR = df_t2_sorted.groupby('BDR_name')['count_registered_stores'].sum().reset_index()
 df_aggregated_t2_BDR_sorted = df_aggregated_t2_BDR.sort_values(by='count_registered_stores', ascending = False)
 kpi2_all_barplot_bdr = px.bar(df_aggregated_t2_BDR_sorted, x='bdr_name', y='count_registered_stores', color_discrete_sequence=['LightSalmon'])
 formatted_max_date_t2 = max_date_t2.strftime('%Y-%m-%d')
