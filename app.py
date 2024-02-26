dc3d32fc0cd78d2b10aad1c552d2b0c6ac4d60bb
Gabriel Lafetá <gabiellafetnascimento@gmail.com>
2024-02-26 10:58:47 -0300

    commit

--- app.py
+++ app.py
@@ -865,7 +865,8 @@ for i, vendor in enumerate(df_t3_pivot.columns):
         y=df_t3_pivot[vendor], 
         name=vendor,
         marker_color=blue_palette[i % len(blue_palette)],  # Use the color palette
-        text=text_labels,  # Use the prepared text labels
+        text=text_labels,
+        texttemplate='%{text:.0f}',
         textposition='outside'  # Position labels outside the bars
     ))
 
