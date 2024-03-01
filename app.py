a22c005060f5faf11ae625eeb7367956ea56ba23
Gabriel Lafetá <gabiellafetnascimento@gmail.com>
2024-03-01 02:49:24 -0300

    commit

--- app.py
+++ app.py
@@ -546,7 +546,7 @@ df_t1['VISIT_DATE'] = pd.to_datetime(df_t1['VISIT_DATE'])
 df_t1_sort_new = df_t1.sort_values(by='VISIT_DATE', ascending=True)
 df_t1_sort_new['FORMATTED_DATE'] = df_t1['VISIT_DATE'].dt.strftime('%d-%b')
 df_t1_stacked = df_t1_sort_new.groupby(['FORMATTED_DATE', 'BDR_name'])['visits_format'].sum().reset_index()
-df_t1_stacked['DATE_FOR_SORTING'] = pd.to_datetime(df_t1_stacked['FORMATTED_DATE'], format='%d-%b')
+df_t1_stacked['DATE_FOR_SORTING'] = pd.to_datetime(df_t1_stacked['FORMATTED_DATE'], format='mixed', errors='coerce')
 df_t1_pivot = df_t1_stacked.pivot(index='DATE_FOR_SORTING', columns='BDR_name', values='visits_format').fillna(0)
 
 df_t1_pivot.index = df_t1_pivot.index.strftime('%d-%b')
