5c2dba7168a55135844fd57c9141210c234dc93f
Gabriel Lafetá <gabiellafetnascimento@gmail.com>
2024-03-18 09:25:37 -0300

    commit

--- app.py
+++ app.py
@@ -3000,16 +3000,16 @@ track_alma_v2 = {
 }
 
 track_alma_df_v2 = pd.DataFrame(track_alma_v2)
-track_alma_df_v2.sort_values(by='Total Visits', inplace=True, ascending=False)
+track_alma_df_v2.sort_values(by='Achieved Adopted %', inplace=True, ascending=False)
 
 sum_row = track_alma_df_v2.sum(numeric_only=True)
 
 totals_row = {'BDR': 'TOTALS'}
 totals_row.update(sum_row.to_dict())
 
-totals_row['Achieved %'] = (sum_row['Total Visits'] / sum_row['Target']) if sum_row['Target'] != 0 else 0
-totals_row['Achieved Register %'] = (sum_row['Total Registers'] / sum_row['Target Register']) if sum_row['Target Register'] != 0 else 0
-totals_row['Achieved Adopted %'] = (sum_row['Total Adopted'] / sum_row['Target Adopted']) if sum_row['Target Adopted'] != 0 else 0
+totals_row['Achieved %'] = (sum_row['Visits LTD'] / sum_row['Target']) if sum_row['Target'] != 0 else 0
+totals_row['Achieved Register %'] = (sum_row['Register LTD'] / sum_row['Target Register']) if sum_row['Target Register'] != 0 else 0
+totals_row['Achieved Adopted %'] = (sum_row['Adoption LTD'] / sum_row['Target Adopted']) if sum_row['Target Adopted'] != 0 else 0
 totals_df = pd.DataFrame([totals_row])
 
 track_alma_df_v2 = pd.concat([track_alma_df_v2, totals_df], ignore_index=True)
