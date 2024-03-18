6d20b54f23da28cfb5ee25a6d0f1acbc905df732
Gabriel Lafetá <gabiellafetnascimento@gmail.com>
2024-03-18 09:35:47 -0300

    commit

--- app.py
+++ app.py
@@ -2868,7 +2868,7 @@ visits8_table_lw_grouped.reset_index(drop=True, inplace=True)
 ############## Adoption
 
 # Visits X GPS
-gps_daily_avg = df_t5_filtrado.groupby('DATE')['GPS'].mean().reset_index()
+gps_daily_avg = df_t5_filtrado.groupby(['DATE', 'BDR Name'])['GPS'].mean().reset_index()
 
 visits_gpsapp_df = pd.merge(df_t1_filtrado, gps_daily_avg, left_on=['BDR Name', 'VISIT_DATE'], right_on=['BDR Name', 'DATE'], how='inner')
 visits_gpsapp_df['VISITS_GPS'] = visits_gpsapp_df['VISITED_STORES'] * visits_gpsapp_df['GPS']
