9bb19eb8347c271f8416ac054de068199d99eb51
Gabriel Lafetá <gabiellafetnascimento@gmail.com>
2024-04-05 09:49:08 -0300

    commit

--- app.py
+++ app.py
@@ -3166,48 +3166,48 @@ merged_df = pd.merge(merged_df, weekly_tasks, on='week_of_year', how='left')
 merged_df = pd.merge(merged_df, weekly_register, on='week_of_year', how='left')
 merged_df_master_table = pd.merge(merged_df, weekly_gps, on='week_of_year', how='left')
 
-### Final master table
-merged_df_master_table['AOV'] = merged_df_master_table['Total_GMV'] / merged_df_master_table['Total_Orders']
+# ### Final master table
+# merged_df_master_table['AOV'] = merged_df_master_table['Total_GMV'] / merged_df_master_table['Total_Orders']
 
-aggregated_values = {}
-for column in merged_df_master_table.columns:
-    if column in ['GPS', 'GPS_QUALITY', 'Task_Effect', 'Completed_Tasks']:
-        aggregated_values[column] = np.mean(merged_df_master_table[column].replace(0, np.nan))
-    else:
-        aggregated_values[column] = merged_df_master_table[column].sum()
+# aggregated_values = {}
+# for column in merged_df_master_table.columns:
+#     if column in ['GPS', 'GPS_QUALITY', 'Task_Effect', 'Completed_Tasks']:
+#         aggregated_values[column] = np.mean(merged_df_master_table[column].replace(0, np.nan))
+#     else:
+#         aggregated_values[column] = merged_df_master_table[column].sum()
 
-aggregated_df = pd.DataFrame(aggregated_values, index=['Accumulated'])
+# aggregated_df = pd.DataFrame(aggregated_values, index=['Accumulated'])
 
-merged_df_master_table_with_accumulated = pd.concat([aggregated_df, merged_df_master_table])
+# merged_df_master_table_with_accumulated = pd.concat([aggregated_df, merged_df_master_table])
 
-for column in merged_df_master_table_with_accumulated.columns[2:]:
-    merged_df_master_table_with_accumulated[column] = pd.to_numeric(merged_df_master_table_with_accumulated[column], errors='coerce')
+# for column in merged_df_master_table_with_accumulated.columns[2:]:
+#     merged_df_master_table_with_accumulated[column] = pd.to_numeric(merged_df_master_table_with_accumulated[column], errors='coerce')
 
-merged_df_master_table_with_accumulated['GMV_Customer'] = merged_df_master_table_with_accumulated['GMV_Customer'].apply(formata_numero)
-merged_df_master_table_with_accumulated['GMV_Force'] = merged_df_master_table_with_accumulated['GMV_Force'].apply(formata_numero)
-merged_df_master_table_with_accumulated['GMV_Grow'] = merged_df_master_table_with_accumulated['GMV_Grow'].apply(formata_numero)
-merged_df_master_table_with_accumulated['Total_GMV'] = merged_df_master_table_with_accumulated['Total_GMV'].apply(formata_numero)
+# merged_df_master_table_with_accumulated['GMV_Customer'] = merged_df_master_table_with_accumulated['GMV_Customer'].apply(formata_numero)
+# merged_df_master_table_with_accumulated['GMV_Force'] = merged_df_master_table_with_accumulated['GMV_Force'].apply(formata_numero)
+# merged_df_master_table_with_accumulated['GMV_Grow'] = merged_df_master_table_with_accumulated['GMV_Grow'].apply(formata_numero)
+# merged_df_master_table_with_accumulated['Total_GMV'] = merged_df_master_table_with_accumulated['Total_GMV'].apply(formata_numero)
 
-merged_df_master_table_with_accumulated['GPS'] = merged_df_master_table_with_accumulated['GPS'].apply(formata_percentual)
-merged_df_master_table_with_accumulated['GPS_QUALITY'] = merged_df_master_table_with_accumulated['GPS_QUALITY'].apply(formata_percentual)
-merged_df_master_table_with_accumulated['Task_Effect'] = merged_df_master_table_with_accumulated['Task_Effect'].apply(formata_percentual)
-merged_df_master_table_with_accumulated['Completed_Tasks'] = merged_df_master_table_with_accumulated['Completed_Tasks'].apply(formata_percentual)
+# merged_df_master_table_with_accumulated['GPS'] = merged_df_master_table_with_accumulated['GPS'].apply(formata_percentual)
+# merged_df_master_table_with_accumulated['GPS_QUALITY'] = merged_df_master_table_with_accumulated['GPS_QUALITY'].apply(formata_percentual)
+# merged_df_master_table_with_accumulated['Task_Effect'] = merged_df_master_table_with_accumulated['Task_Effect'].apply(formata_percentual)
+# merged_df_master_table_with_accumulated['Completed_Tasks'] = merged_df_master_table_with_accumulated['Completed_Tasks'].apply(formata_percentual)
 
-merged_df_master_table_sorted = merged_df_master_table_with_accumulated.sort_values(by='week_of_year', ascending=False).fillna(0)
-merged_df_master_table_sorted = merged_df_master_table_sorted[~merged_df_master_table_sorted['week_of_year'].isin([3, 4])]
-merged_df_master_table_sorted.columns = merged_df_master_table_sorted.columns.str.replace('_', ' ')
-merged_df_master_table_sorted = merged_df_master_table_sorted.set_index(merged_df_master_table_sorted.columns[0])
+# merged_df_master_table_sorted = merged_df_master_table_with_accumulated.sort_values(by='week_of_year', ascending=False).fillna(0)
+# merged_df_master_table_sorted = merged_df_master_table_sorted[~merged_df_master_table_sorted['week_of_year'].isin([3, 4])]
+# merged_df_master_table_sorted.columns = merged_df_master_table_sorted.columns.str.replace('_', ' ')
+# merged_df_master_table_sorted = merged_df_master_table_sorted.set_index(merged_df_master_table_sorted.columns[0])
 
-merged_df_master_table_sorted = merged_df_master_table_sorted.rename(index={merged_df_master_table_sorted.index[0]: "Accumulated"})
-merged_df_master_table_sorted.iloc[0, 0] = "Launch"
+# merged_df_master_table_sorted = merged_df_master_table_sorted.rename(index={merged_df_master_table_sorted.index[0]: "Accumulated"})
+# merged_df_master_table_sorted.iloc[0, 0] = "Launch"
 
-merged_df_master_table_sorted.fillna(0, inplace=True)
+# merged_df_master_table_sorted.fillna(0, inplace=True)
 
-columns_master_table = merged_df_master_table_sorted.columns
-merged_df_master_table_sorted_cv = merged_df_master_table_sorted.to_csv(index=False).encode('utf-8')
+# columns_master_table = merged_df_master_table_sorted.columns
+# merged_df_master_table_sorted_cv = merged_df_master_table_sorted.to_csv(index=False).encode('utf-8')
 
-master_table = style_table(merged_df_master_table_sorted, columns_master_table)
-master_table_html = master_table.to_html()
+# master_table = style_table(merged_df_master_table_sorted, columns_master_table)
+# master_table_html = master_table.to_html()
 
 ####### KPI track Table
 ### Tabela Buyers
@@ -3395,172 +3395,6 @@ for bdr_key, bdr_name in BDR_dict.items():
 
 register_table_semana_atual.sort_values(by='BDR Name', inplace=True)
 register_table_semana_atual.reset_index(drop=True, inplace=True)
-
-
-### DF consolidado
-adopted_last_day_key = f"{last_day.strftime('%d-%m')}"
-adopted_yesterday_day_key = f"{penultimo_dia.strftime('%d-%m')}"
-
-track_alma = {
-    "BDR": buyers_table["BDR Name"].tolist(),
-    "Adopted": buyers_table["Total_Buyers"].tolist(),
-    f"Adopted {adopted_last_day_key}": buyers_table_lastday["Total_Buyers"].tolist(),
-    f"Adopted {adopted_yesterday_day_key}": buyers_table_penultimo["Total_Buyers"].tolist(),
-    "Adopted Current Week": buyers_table_semana_atual["Total_Buyers"].tolist(),
-
-    f"Orders {adopted_last_day_key}": buyers_table_lastday["Total_Orders"].tolist(),
-    f"Orders {adopted_yesterday_day_key}": buyers_table_penultimo["Total_Orders"].tolist(),
-    "Orders Current Week": buyers_table_semana_atual["Total_Orders"].tolist(),
-    "Orders LTD": buyers_table["Total_Orders"].tolist(),
-
-    f"GMV {adopted_last_day_key}": buyers_table_lastday["Total_GMV"].tolist(),
-    f"GMV {adopted_yesterday_day_key}": buyers_table_penultimo["Total_GMV"].tolist(),
-    "GMV Current Week": buyers_table_semana_atual["Total_GMV"].tolist(),
-    "GMV LTD": buyers_table["Total_GMV"].tolist(),
-
-    f"Register {adopted_last_day_key}": registers_table_lastday["Total_Registers"].fillna(0).tolist(),
-    f"Register {adopted_yesterday_day_key}": registers_table_penultimo["Total_Registers"].fillna(0).tolist(),
-    "Register Current Week": register_table_semana_atual["Total_Registers"].fillna(0).tolist(),
-    "Register LTD": register_table["Total_Registers"].fillna(0).tolist()
-
-}
-
-track_alma_df = pd.DataFrame(track_alma)
-track_alma_df.sort_values(by='Adopted', inplace=True, ascending=False)
-
-sum_row = track_alma_df.sum(numeric_only=True)
-totals_row = {'BDR': 'TOTALS'}
-totals_row.update(sum_row.to_dict())
-totals_df = pd.DataFrame([totals_row])
-
-track_alma_df = pd.concat([track_alma_df, totals_df], ignore_index=True)
-
-gmv_columns = [col for col in track_alma_df.columns if 'GMV' in col]
-for col in gmv_columns:
-    track_alma_df[col] = track_alma_df[col].apply(formata_numero)
-
-track_alma_df.set_index(track_alma_df.columns[0], inplace=True)
-
-
-#### New Styler
-
-def style_table_2(df, columns, font_size='10pt'):
-    def format_with_dots(value):
-        if isinstance(value, (int, float)):
-            return '{:,.0f}'.format(value).replace(',', '.')
-        return value
-
-    # Aplicando a formatação com pontos para os valores numéricos
-    styler = df.style.format(format_with_dots, subset=columns)\
-        .set_table_styles([
-            # Estilo do cabeçalho
-            {'selector': 'thead th',
-             'props': [('background-color', '#1a2634'), ('color', 'white'), ('font-weight', 'bold')]},
-            # Alinhamento dos dados na célula
-            {'selector': 'td',
-             'props': [('text-align', 'center')]},
-            # Estilo da fonte e tamanho para toda a tabela
-            {'selector': 'table, th, td',
-             'props': [('font-size', font_size)]},
-            # Removendo linhas de grade
-            {'selector': 'table',
-             'props': [('border-collapse', 'collapse'), ('border-spacing', '0'), ('border', '0')]}
-        ])
-
-    # Adicionando bordas grossas a cada 4 colunas, começando na terceira coluna
-    for col in range(1, len(df.columns), 4):
-        styler = styler.set_table_styles([
-            {'selector': f'td:nth-child({col})',
-             'props': [('border-right', '2px solid black')]}
-        ], overwrite=False, axis=1)
-
-    # Estilizando a última linha com fundo preto e fonte amarela
-    styler = styler.set_properties(**{'background-color': '#1a2634', 'color': 'white'}, subset=pd.IndexSlice[df.index[-1], :])
-
-    return styler
-
-alma_csv = track_alma_df.to_csv(index=False).encode('utf-8')
-master_table_2 = style_table_2(track_alma_df, track_alma_df.columns)
-master_table_2_html = master_table_2.to_html()
-
-#------------------------------------------------------------------------------------------------------
-#### New Styler
-
-def style_table_3(df, columns, font_size='10pt'):
-    def format_with_dots(value):
-        if isinstance(value, (int, float)):
-            return '{:,.0f}'.format(value).replace(',', '.')
-        return value
-
-    # Applying formatting with dots for numeric values
-    styler = df.style.format(format_with_dots, subset=columns)\
-        .set_table_styles([
-            # Header style
-            {'selector': 'thead th',
-             'props': [('background-color', '#1a2634'), ('color', 'white'), ('font-weight', 'bold')]},
-            # Cell data alignment
-            {'selector': 'td',
-             'props': [('text-align', 'center')]},
-            # Font style and size for the entire table
-            {'selector': 'table, th, td',
-             'props': [('font-size', font_size)]},
-            # Removing grid lines
-            {'selector': 'table',
-             'props': [('border-collapse', 'collapse'), ('border-spacing', '0'), ('border', '0')]}
-        ])
-
-    # Adding thick borders every 5 columns, except for the last 6 columns
-    total_columns = len(df.columns)
-    for col in range(1, total_columns - 6, 5):  # Start from the 5th column and skip every 5 columns, avoiding the last 6
-        styler = styler.set_table_styles([
-            {'selector': f'td:nth-child({col}), th:nth-child({col})',
-             'props': [('border-right', '2px solid black')]}
-        ], overwrite=False, axis=1)
-
-    # Styling the last row with a black background and white font
-    styler = styler.set_properties(**{'background-color': '#1a2634', 'color': 'white'}, subset=pd.IndexSlice[df.index[-1], :])
-
-    return styler
-
-def style_table_4(df, columns, font_size='10pt'):
-    def format_with_dots(value):
-        if isinstance(value, (int, float)):
-            return '{:,.0f}'.format(value).replace(',', '.')
-        return value
-
-    # Aplicando a formatação com pontos para os valores numéricos
-    styler = df.style.format(format_with_dots, subset=columns)\
-        .set_table_styles([
-            # Estilo do cabeçalho
-            {'selector': 'thead th',
-             'props': [('background-color', '#1a2634'), ('color', 'white'), ('font-weight', 'bold')]},
-            # Alinhamento dos dados na célula
-            {'selector': 'td',
-             'props': [('text-align', 'center')]},
-            # Estilo da fonte e tamanho para toda a tabela
-            {'selector': 'table, th, td',
-             'props': [('font-size', font_size)]},
-            # Removendo linhas de grade
-            {'selector': 'table',
-             'props': [('border-collapse', 'collapse'), ('border-spacing', '0'), ('border', '0')]}
-        ])
-
-    # Adicionando bordas grossas a cada 6 colunas, começando na terceira coluna (índice 3)
-    for col in range(3, len(df.columns) + 1, 5):
-        styler = styler.set_table_styles([
-            {'selector': f'td:nth-child({col})',
-             'props': [('border-right', '2px solid black')]}
-        ], overwrite=False, axis=1)
-
-    # Estilizando a última coluna com a mesma cor do restante
-    styler = styler.set_properties(**{'background-color': '#1a2634', 'color': 'white'}, subset=pd.IndexSlice[:, df.columns[-1]])
-
-    # Aplicando um mapa de calor às colunas % com gradiente de verde para valores altos, amarelo para médios e vermelho para baixos
-    percent_columns = [col for col in df.columns if '%' in col]
-    if percent_columns:
-        styler = styler.background_gradient(subset=percent_columns, cmap='RdYlGn', low=0.1, high=0.5)
-
-    return styler
 
 ### Tabela v2
 
