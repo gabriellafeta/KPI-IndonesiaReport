88d6ec7a56c5d29b8ba4b37b185d612613ccb026
Gabriel Lafetá <gabiellafetnascimento@gmail.com>
2024-04-17 13:51:41 -0300

    commit

--- app.py
+++ app.py
@@ -2630,7 +2630,7 @@ visits_gpsapp_df_ld_grouped_all.reset_index(drop=True, inplace=True)
 ##### Semana
 
 visits_gpsapp_df_all['week_of_year'] = visits_gpsapp_df_all['VISIT_DATE'].dt.isocalendar().week
-data_final = datetime.now()
+data_final = visits_gpsapp_df_all['week_of_year'].max()
 current_week_number = data_final.isocalendar()[1]
 visits_gpsapp_df_lw_all = visits_gpsapp_df_all[visits_gpsapp_df_all['week_of_year'] == current_week_number]
 
