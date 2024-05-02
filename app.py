0de026f9c633e7dd49f6ef488fa4c8b0f31abe78
Gabriel Lafetá <gabiellafetnascimento@gmail.com>
2024-05-02 10:44:25 -0300

    commit

--- app.py
+++ app.py
@@ -3234,26 +3234,26 @@ adopted_last_day_key = f"{last_day.strftime('%d-%m')}"
 adopted_yesterday_day_key = f"{penultimo_dia.strftime('%d-%m')}"
 
 track_alma = {
-    # "BDR": buyers_table["BDR Name"].tolist(),
-    # "Adopted": buyers_table["Total_Buyers"].tolist(),
-    # f"Adopted {adopted_last_day_key}": buyers_table_lastday["Total_Buyers"].tolist(),
-    # f"Adopted {adopted_yesterday_day_key}": buyers_table_penultimo["Total_Buyers"].tolist(),
-    # "Adopted Current Week": buyers_table_semana_atual["Total_Buyers"].tolist(),
+    "BDR": buyers_table["BDR Name"].tolist(),
+    "Adopted": buyers_table["Total_Buyers"].tolist(),
+    f"Adopted {adopted_last_day_key}": buyers_table_lastday["Total_Buyers"].tolist(),
+    f"Adopted {adopted_yesterday_day_key}": buyers_table_penultimo["Total_Buyers"].tolist(),
+    "Adopted Current Week": buyers_table_semana_atual["Total_Buyers"].tolist(),
 
-    # f"Orders {adopted_last_day_key}": buyers_table_lastday["Total_Orders"].tolist(),
-    # f"Orders {adopted_yesterday_day_key}": buyers_table_penultimo["Total_Orders"].tolist(),
-    # "Orders Current Week": buyers_table_semana_atual["Total_Orders"].tolist(),
-    # "Orders LTD": buyers_table["Total_Orders"].tolist(),
+    f"Orders {adopted_last_day_key}": buyers_table_lastday["Total_Orders"].tolist(),
+    f"Orders {adopted_yesterday_day_key}": buyers_table_penultimo["Total_Orders"].tolist(),
+    "Orders Current Week": buyers_table_semana_atual["Total_Orders"].tolist(),
+    "Orders LTD": buyers_table["Total_Orders"].tolist(),
 
-    # f"GMV {adopted_last_day_key}": buyers_table_lastday["Total_GMV"].tolist(),
-    # f"GMV {adopted_yesterday_day_key}": buyers_table_penultimo["Total_GMV"].tolist(),
-    # "GMV Current Week": buyers_table_semana_atual["Total_GMV"].tolist(),
-    # "GMV LTD": buyers_table["Total_GMV"].tolist(),
+    f"GMV {adopted_last_day_key}": buyers_table_lastday["Total_GMV"].tolist(),
+    f"GMV {adopted_yesterday_day_key}": buyers_table_penultimo["Total_GMV"].tolist(),
+    "GMV Current Week": buyers_table_semana_atual["Total_GMV"].tolist(),
+    "GMV LTD": buyers_table["Total_GMV"].tolist(),
 
-    # f"Register {adopted_last_day_key}": registers_table_lastday["Total_Registers"].fillna(0).tolist(),
-    # f"Register {adopted_yesterday_day_key}": registers_table_penultimo["Total_Registers"].fillna(0).tolist(),
-    # "Register Current Week": register_table_semana_atual["Total_Registers"].fillna(0).tolist(),
-    # "Register LTD": register_table["Total_Registers"].fillna(0).tolist()
+    f"Register {adopted_last_day_key}": registers_table_lastday["Total_Registers"].fillna(0).tolist(),
+    f"Register {adopted_yesterday_day_key}": registers_table_penultimo["Total_Registers"].fillna(0).tolist(),
+    "Register Current Week": register_table_semana_atual["Total_Registers"].fillna(0).tolist(),
+    "Register LTD": register_table["Total_Registers"].fillna(0).tolist()
 
 }
 
