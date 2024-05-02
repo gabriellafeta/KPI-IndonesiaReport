56b94b92eed8bde9917c5d92575db6896f77e844
Gabriel Lafetá <gabiellafetnascimento@gmail.com>
2024-05-02 06:54:57 -0300

    commit

--- app.py
+++ app.py
@@ -3242,7 +3242,7 @@ track_alma = {
 
     f"Orders {adopted_last_day_key}": buyers_table_lastday["Total_Orders"].tolist(),
     f"Orders {adopted_yesterday_day_key}": buyers_table_penultimo["Total_Orders"].tolist(),
-    #"Orders Current Week": buyers_table_semana_atual["Total_Orders"].tolist(),
+    "Orders Current Week": buyers_table_semana_atual["Total_Orders"].tolist(),
     "Orders LTD": buyers_table["Total_Orders"].tolist(),
 
     f"GMV {adopted_last_day_key}": buyers_table_lastday["Total_GMV"].tolist(),
