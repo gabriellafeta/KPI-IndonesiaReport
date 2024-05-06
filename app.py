89a86fe115d6a32761fff4e2b2a5b5a0d4d35f52
Gabriel Lafetá <gabiellafetnascimento@gmail.com>
2024-05-06 11:32:13 -0300

    commit

--- app.py
+++ app.py
@@ -4341,8 +4341,14 @@ track_alma_v3 = {
 
 }
 
-valid_indices = [i for i, bdr in enumerate(track_alma_v3['BDR']) if bdr.strip()]
-filtered_track_alma_v3 = {key: [value[i] for i in valid_indices] for key, value in track_alma_v3.items()}
+# valid_indices = [i for i, bdr in enumerate(track_alma_v3['BDR']) if bdr.strip()]
+# filtered_track_alma_v3 = {key: [value[i] for i in valid_indices] for key, value in track_alma_v3.items()}
+
+filtered_track_alma_v3 = {
+    key: [value[i] for i in valid_indices if i < len(value)]
+    for key, value in track_alma_v3.items()
+}
+
 
 track_alma_df_v3 = pd.DataFrame(filtered_track_alma_v3)
 track_alma_df_v3.sort_values(by='Achieved Customers Adopted %', inplace=True, ascending=False)
